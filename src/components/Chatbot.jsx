10f60a1d8c30518a0d9459d9d26d925278432f86
Chirag Soni <csoni0693@gmail.com>
2025-10-04 07:51:34 +0530

    hey

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -4,7 +4,7 @@ import { assets } from "../assets/assets";
 const Chatbot = () => {
   return (
     <a
-      href="https://9000-firebase-studio-1759501192823.cluster-bqwaigqtxbeautecnatk4o6ynk.cloudworkstations.dev"
+      href="https://chatbot-ai-inventory.onrender.com/"
       target="_blank"
       className="fixed bottom-10 right-10 w-15 cursor-pointer bg-slate-700 rounded-full h-15 animate-bounce "
     >
